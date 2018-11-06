5b4e21d72db6f8326645c0052a035f881ad0d6e9
liusiyuan <liusiyuan@madhouse-inc.com>
2018-11-06 14:30:57 +0800

    add sample accounts configuration

---
+++ manageraccounts/accounts/example_appannie_accounts.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+accounts = [
+ {
+        'no': '1',
+        'email': 'abc@gmail.com',
+        'pwd': 'passwd'
+ },
+ {
+        'no': '2',
+        'email': 'cba@gmail.com',
+        'pwd': 'passwd'
+ }
+]
